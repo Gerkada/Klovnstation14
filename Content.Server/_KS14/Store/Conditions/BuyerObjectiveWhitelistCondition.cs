09a033984980928ee1e8a08ff56910e1ecec88b3
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-12-09 00:58:10 +0800

    f

--- Content.Server/_KS14/Store/Conditions/BuyerObjectiveWhitelistCondition.cs
+++ Content.Server/_KS14/Store/Conditions/BuyerObjectiveWhitelistCondition.cs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Gerkada
 // SPDX-FileCopyrightText: 2025 github_actions[bot]
 //
-// SPDX-License-Identifier: MPL-2.0
+// SPDX-License-Identifier: MIT
 
 using Content.Shared.Mind;
 using Content.Shared.Store;
