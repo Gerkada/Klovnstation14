068f93b4996c442f76a9bb127f7940bc26b0945d
Gerkada <solider.23.43@gmail.com>
2025-12-21 17:06:02 +0200

    licensing o algo (idk if I should change licensing of files outside of our namespace btw)

--- Content.Server/_KS14/Atmos/Reactions/ArgonAbsorptionReaction.cs
+++ Content.Server/_KS14/Atmos/Reactions/ArgonAbsorptionReaction.cs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Syndicate Engineer
 // SPDX-FileCopyrightText: 2025 syndicate-engineer
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 using Content.Server.Atmos;
 using Content.Server.Atmos.EntitySystems;
