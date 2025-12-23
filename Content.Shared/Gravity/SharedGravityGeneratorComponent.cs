22153b11c1436a95ff6e91b12b7055a692466e97
nabegator220 <nabe@gator.lol>
2025-12-23 17:38:11 +0100

    Revert "Gravity Generators cannot be unanchored while active (#41256)"
    This reverts commit 4f0b1f377eeb8533488ebf0ecb2a0bc7eabc7609.

--- Content.Shared/Gravity/SharedGravityGeneratorComponent.cs
+++ Content.Shared/Gravity/SharedGravityGeneratorComponent.cs
@@ -3,45 +3,42 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.Gravity;
 
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class GravityGeneratorComponent : Component
+[NetworkedComponent()]
+[Virtual]
+public partial class SharedGravityGeneratorComponent : Component
 {
-    [DataField] public float LightRadiusMin { get; set; }
-    [DataField] public float LightRadiusMax { get; set; }
-
     /// <summary>
     /// A map of the sprites used by the gravity generator given its status.
     /// </summary>
-    [DataField, Access(typeof(SharedGravitySystem))]
-    public Dictionary<PowerChargeStatus, string> SpriteMap = [];
+    [DataField("spriteMap")]
+    [Access(typeof(SharedGravitySystem))]
+    public Dictionary<PowerChargeStatus, string> SpriteMap = new();
 
     /// <summary>
     /// The sprite used by the core of the gravity generator when the gravity generator is starting up.
     /// </summary>
-    [DataField]
+    [DataField("coreStartupState")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string CoreStartupState = "startup";
 
     /// <summary>
     /// The sprite used by the core of the gravity generator when the gravity generator is idle.
     /// </summary>
-    [DataField]
+    [DataField("coreIdleState")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string CoreIdleState = "idle";
 
     /// <summary>
     /// The sprite used by the core of the gravity generator when the gravity generator is activating.
     /// </summary>
-    [DataField]
+    [DataField("coreActivatingState")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string CoreActivatingState = "activating";
 
     /// <summary>
     /// The sprite used by the core of the gravity generator when the gravity generator is active.
     /// </summary>
-    [DataField]
+    [DataField("coreActivatedState")]
+    [ViewVariables(VVAccess.ReadWrite)]
     public string CoreActivatedState = "activated";
-
-    /// <summary>
-    /// Is the gravity generator currently "producing" gravity?
-    /// </summary>
-    [DataField, AutoNetworkedField, Access(typeof(SharedGravityGeneratorSystem))]
-    public bool GravityActive = false;
 }
