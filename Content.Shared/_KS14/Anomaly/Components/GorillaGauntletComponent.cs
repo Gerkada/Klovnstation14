e112d7391fb6b44633cf5ab3f074c369694b9cc0
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-12-31 04:07:46 +0300

    Update GorillaGauntletComponent.cs

--- Content.Shared/_KS14/Anomaly/Components/GorillaGauntletComponent.cs
+++ Content.Shared/_KS14/Anomaly/Components/GorillaGauntletComponent.cs
@@ -8,6 +8,4 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._KS14.Anomaly.Components;
 
 [RegisterComponent, NetworkedComponent]
-public sealed partial class GorillaGauntletComponent : Component
-{
-}
+public sealed partial class GorillaGauntletComponent : Component;
