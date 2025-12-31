007e7c5764588c1cab6d29ace16d838662128066
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-12-31 04:07:28 +0300

    Update GorillaDisposalComponent.cs

--- Content.Shared/_KS14/Anomaly/Components/GorillaDisposalComponent.cs
+++ Content.Shared/_KS14/Anomaly/Components/GorillaDisposalComponent.cs
@@ -11,6 +11,4 @@ namespace Content.Shared._KS14.Anomaly.Components;
 /// A tag component that allows an entity to be shoved into a disposal unit by the G.O.R.I.L.L.A. gauntlet.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class GorillaDisposalComponent : Component
-{
-}
+public sealed partial class GorillaDisposalComponent : Component;
